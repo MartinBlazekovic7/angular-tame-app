7986ca5861ebbe0118cf0a813e11395ff9435c03
Martin Blažeković <martinblazekovic7@gmail.com>
2023-03-19 01:24:47 +0100

    #5 - add bootstrap, routerLinks, profile page, favicon and title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
+import { RegisterPageComponent } from './components/pages/register-page/register-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { SearchPageComponent } from './components/pages/search-page/search-page.component';
+import { SettingsPageComponent } from './components/pages/settings-page/settings-page.component';
+import { ProfilePageComponent } from './components/pages/profile-page/profile-page.component';
 import { HomePageComponent } from './components/pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: HomePageComponent },
+  { path: 'home', component: HomePageComponent },
+  { path: 'profile', component: ProfilePageComponent },
+  { path: 'settings', component: SettingsPageComponent },
+  { path: 'search', component: SearchPageComponent },
+  { path: 'login', component: LoginPageComponent },
+  { path: 'register', component: RegisterPageComponent },
   { path: '**', redirectTo: '' },
 ];
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -5,7 +6,7 @@ import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [NavbarComponent, FooterComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
